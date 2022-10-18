283f67d4ae7bf5d09621a6738b6a220c92451f52
Michel PC <0801342@csfoy.ca>
2022-10-18 17:49:56 -0400

    Création des balises générales de fiche/index.php

--- artistes/fiche/index.php
+++ artistes/fiche/index.php
@@ -15,13 +15,31 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Fiche "artistes/fiche/index.php</title>
+    <link rel="stylesheet" href="css/styles.css" media="all">
 </head>
 
 <body>
-    <h1>Fiche "artistes/fiche/index.php</h1>
-    <header><?php include($niveau . "inc/scripts/header.inc.php"); ?></header>
-    <main>Contenu principal</main>
-    <footer><?php include($niveau . "inc/scripts/footer.inc.php"); ?></footer>
+<div class="website">
+    <header class="header full-bleed" role="banner">
+        <header><?php include($niveau . "inc/scripts/header.inc.php"); ?></header>
+        <p>Lorem Elsass ipsum lacus leverwurscht Wurschtsalad mamsell Gal. gewurztraminer turpis, suspendisse commodo Oberschaeffolsheim ornare aliquam semper Miss Dahlias Mauris turpis sagittis kuglopf eleifend dignissim baeckeoffe geht's Richard Schirmeck mollis
+            habitant schnaps ante et sit leo schpeck sit Salu bissame Salut bisamme varius quam.</p>
+    </header>
+
+    <main id="main" role="main" class="main">
+        <h1>Nom Artiste</h1>
+        <a href="">Site Web</a>
+        <img src="https://i.picsum.photos/id/386/960/490.jpg?hmac=RcIYBU3QIXDOP7NMdRKxaWlzf3izkxtM81zazZgricw" alt="">
+        <p>Yoo ch'ai lu dans les DNA que le Racing a encore perdu contre Oberschaeffolsheim. Verdammi et moi ch'avais donc parié deux knacks et une flammekueche. Ah so ? T'inquiète, ch'ai ramené du schpeck, du chambon, un kuglopf et du schnaps dans mon rucksack. Allez, s'guelt ! Wotch a kofee avec ton bibalaekaess et ta wurscht ? Yeuh non che suis au réchime, je ne mange plus que des Grumbeere light et che fais de la chym avec Chulien. Tiens, un rottznoz sur le comptoir.</p>
+        <p>Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ? Yeuh non, merci vielmols mais che dois partir à la Coopé de Truchtersheim acheter des mänele et des rossbolla pour les gamins. Hopla tchao bissame ! Consectetur adipiscing elit amet elementum nullam bissame bredele Heineken picon bière gal sed risus, condimentum Verdammi ch'ai ac réchime météor barapli s'guelt quam, non Christkindelsmärik blottkopf, Carola tellus rucksack vielmols, Gal !</p>
+    </main>
+
+    <footer class="footer full-bleed" role="contentinfo">
+        <footer><?php include($niveau . "inc/scripts/footer.inc.php"); ?></footer>
+        <p>Lorem Elsass ipsum lacus leverwurscht Wurschtsalad mamsell Gal. gewurztraminer turpis, suspendisse commodo Oberschaeffolsheim ornare aliquam semper Miss Dahlias Mauris turpis sagittis kuglopf, Gal !</p>
+    </footer>
+
+</div>
 </body>
 
 </html>
