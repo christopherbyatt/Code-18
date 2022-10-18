5ebeabc6517ee32c6ceda4ca31ee06969f324c43
Michel PC <0801342@csfoy.ca>
2022-10-18 16:38:04 -0400

    Tests index pour connection local

--- Index.php
+++ Index.php
@@ -1,7 +1,20 @@
 <<<<<<< HEAD
 <?php 
-<h1> </h1>
+
 ?>
 =======
 <?php ?>
 >>>>>>> 4a9398d2401aa82f20977c3df4f74730b80dec46
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>TEST</h1>
+</body>
+</html>
