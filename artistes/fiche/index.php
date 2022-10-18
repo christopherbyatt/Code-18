c320ca97b62160a7a056866ed08fe3851dbe9223
Michel PC <0801342@csfoy.ca>
2022-10-18 17:18:54 -0400

    Début de structuration html des fichiers p-fiche et fiche/index.php

--- artistes/fiche/index.php
+++ artistes/fiche/index.php
@@ -1,1 +1,24 @@
-<?php ?>
+<?php
+    ini_set('display_errors',1);
+
+    //Définition de la variable niveau
+    $niveau='../../';
+    //Inclusion du fichier de configuration
+    include($niveau . "inc/fragment/config.inc-michel.php");
+    //include($niveau . "inc/fragment/config.inc-michel.php");
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fiche "artistes/fiche/index.php</title>
+</head>
+<body>
+    <h1>Fiche "artistes/fiche/index.php</h1>
+</body>
+</html>
