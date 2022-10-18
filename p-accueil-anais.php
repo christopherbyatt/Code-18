7c22419a78af4529c3dcf0a4fff9e1360e1c7d0d
Anaïs Mannée <anaismannee@gmail.com>
2022-10-18 18:00:09 -0400

    v1 integration liste des artistes

---
+++ p-accueil-anais.php
@@ -0,0 +1,72 @@
+<?php
+echo "Bonjour";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+    <header>
+        <img src="" alt="">
+        <nav>
+            <ul>
+                <li>Le OFF</li>
+                <li>Programmation</li>
+                <li>Artistes</li>
+                <li>Partenaires</li>
+            </ul>
+        </nav>
+        <button type="button">Acheter mon passeport</button>
+    </header>
+    <main>
+        <h1>Liste des artistes</h1>
+        <section>
+            <div>
+                <figure>
+                    <img src="" alt="">
+                    <figcaption>Cabaret Olibrius</figcaption>
+                </figure>
+                <figure>
+                    <img src="" alt="">
+                    <figcaption>Diamond Rings</figcaption>
+                </figure>
+                <figure>
+                    <img src="" alt="">
+                    <figcaption>Jah & I</figcaption>
+                </figure>
+                <button type="button">arriere</button>
+                <button type="button">avant</button>
+            </div>
+        </section>
+    </main>
+
+        <footer>
+            <div>
+                <img src="" alt="">
+                <p>110 boulevard René-Lévesque Ouest<br>C.P. 48036<br>QC, Québec, G1R 5R5</p>
+            </div>
+            <nav>
+                <ul>
+                    <li>Le OFF</li>
+                    <li>Programmation</li>
+                    <li>Artistes</li>
+                    <li>Partenaires</li>
+                </ul>
+            </nav>
+            <div>
+                <button type="button">Acheter mon passeport</button>
+                <p>du 6 juillet au 10 juillet 2022</p>
+                <p>Formulaire d’abonnement à l’infolettre du festival</p>
+                <div class="reseauxSociaux">
+                    <img src="" alt="">
+                    <img src="" alt="">
+                    <img src="" alt="">
+                </div>
+            </div>
+            <p>© 2009-2022 Festival OFF Tous droits réservés</p>
+        </footer>
+    </body>
+</html>
+
