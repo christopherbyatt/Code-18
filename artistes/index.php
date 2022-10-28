1fa64962a4f78740278813c76ef874f197b272b1
Anaïs Mannée <anaismannee@gmail.com>
2022-10-28 13:37:19 -0400

    essai v2

--- artistes/index.php
+++ artistes/index.php
@@ -1,1 +1,68 @@
-<?php ?>
+<?php
+;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../css/style-anais.css">
+</head>
+<body>
+<header class="header" role="banner">
+    <img class="header__logo" src="../images/logoOff_noir.png" alt="accueil">
+    <ul class="nav">
+        <li class="nav__item"><a class="nav__lien">Le OFF</a></li>
+        <li class="nav__item"><a class="nav__lien">Programmation</a></li>
+        <li class="nav__item"><a class="nav__lien">Artistes</a></li>
+        <li class="nav__item"><a class="nav__lien">Partenaires</a></li>
+    </ul>
+    <button class="header__btn" type="button">Acheter mon passeport</button>
+</header>
+<main class="main" role="main">
+    <h1 class="titrePrincipal">Liste des artistes</h1>
+    <section class="artistes">
+        <div>
+            <figure class="infoArtiste">
+                <img class="infoArtiste__img" src="" alt="">
+                <figcaption class="infoArtiste__nom">Cabaret Olibrius</figcaption>
+            </figure>
+            <figure class="infoArtiste">
+                <img class="infoArtiste__img" src="" alt="">
+                <figcaption class="infoArtiste__nom">Diamond Rings</figcaption>
+            </figure>
+            <figure class="infoArtiste">
+                <img class="infoArtiste__img" src="" alt="">
+                <figcaption class="infoArtiste__nom">Jah & I</figcaption>
+            </figure>
+            <button class="arriere" type="button">arriere</button>
+            <button class="avant" type="button">avant</button>
+        </div>
+    </section>
+</main>
+
+<footer class="footer" role="contentinfo">
+    <div class="haut">
+        <img class="footer__logo" src="../images/off_jaune.svg" alt="accueil">
+        <p class="adresse">110 boulevard René-Lévesque Ouest<br>C.P. 48036<br>QC, Québec, G1R 5R5</p>
+    </div>
+    <ul class="nav">
+        <li class="menu__item"><a class="nav__lien">Le OFF</a></li>
+        <li class="nav__item"><a class="nav__lien">Programmation</a></li>
+        <li class="nav__item"><a class="nav__lien">Artistes</a></li>
+        <li class="nav__item"><a class="nav__lien">Partenaires</a></li>
+    </ul>
+    <div class="informations">
+        <button class="informations__btn" type="button">Acheter mon passeport</button>
+        <p class="informations__p">du 6 juillet au 10 juillet 2022</p>
+        <a class="information__lien" href="">Formulaire d’abonnement à l’infolettre du festival</a>
+        <div class="reseaux">
+            <a class="reseaux__lien" href="facebook.com"><img class="reseaux_img" src="" alt="facebook"></a>
+            <a class="reseaux__lien" href="twitter.com"><img class="reseaux_img" src="" alt="twitter"></a>
+            <a class="reseaux__lien" href="youtube.com"><img class="reseaux_img" src="" alt="youtube"></a>
+        </div>
+    </div>
+    <p class="footer__copyright">© 2009-2022 Festival OFF Tous droits réservés</p>
+</footer>
+</body>
+</html>
+
