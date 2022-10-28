fd2294db5c7e3ed15a559d39d256930f58fa18e5
Michel PC <0801342@csfoy.ca>
2022-10-28 14:22:35 -0400

    Ajustement header dans un include et création de styles pour les fiches artiste

--- artistes/fiche/index.php
+++ artistes/fiche/index.php
@@ -15,23 +15,32 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Fiche "artistes/fiche/index.php</title>
-    <link rel="stylesheet" href="css/styles.css" media="all">
+    <link rel="stylesheet" href='../../css/style-michel.css' media="all">
 </head>
 
 <body>
 <div class="website">
-    <header class="header full-bleed" role="banner">
-        <header><?php include($niveau . "inc/scripts/header.inc.php"); ?></header>
-        <p>Lorem Elsass ipsum lacus leverwurscht Wurschtsalad mamsell Gal. gewurztraminer turpis, suspendisse commodo Oberschaeffolsheim ornare aliquam semper Miss Dahlias Mauris turpis sagittis kuglopf eleifend dignissim baeckeoffe geht's Richard Schirmeck mollis
-            habitant schnaps ante et sit leo schpeck sit Salu bissame Salut bisamme varius quam.</p>
-    </header>
+    <header><?php include($niveau . "inc/scripts/header.inc.php"); ?></header>
 
     <main id="main" role="main" class="main">
         <h1>Nom Artiste</h1>
         <a href="">Site Web</a>
         <img src="https://i.picsum.photos/id/386/960/490.jpg?hmac=RcIYBU3QIXDOP7NMdRKxaWlzf3izkxtM81zazZgricw" alt="">
+
+        <h2>Description</h2>
         <p>Yoo ch'ai lu dans les DNA que le Racing a encore perdu contre Oberschaeffolsheim. Verdammi et moi ch'avais donc parié deux knacks et une flammekueche. Ah so ? T'inquiète, ch'ai ramené du schpeck, du chambon, un kuglopf et du schnaps dans mon rucksack. Allez, s'guelt ! Wotch a kofee avec ton bibalaekaess et ta wurscht ? Yeuh non che suis au réchime, je ne mange plus que des Grumbeere light et che fais de la chym avec Chulien. Tiens, un rottznoz sur le comptoir.</p>
-        <p>Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ? Yeuh non, merci vielmols mais che dois partir à la Coopé de Truchtersheim acheter des mänele et des rossbolla pour les gamins. Hopla tchao bissame ! Consectetur adipiscing elit amet elementum nullam bissame bredele Heineken picon bière gal sed risus, condimentum Verdammi ch'ai ac réchime météor barapli s'guelt quam, non Christkindelsmärik blottkopf, Carola tellus rucksack vielmols, Gal !</p>
+
+        <h2>Provenance</h2>
+        <p>Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ?</p>
+
+        <h2>Style musical</h2>
+        <p>Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ?</p>
+
+        <h2>Découvrir d'autres artistes</h2>
+        <p>Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ?</p>
+
+        <h2>Provenance</h2>
+        <p>Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ?</p>
     </main>
 
     <footer class="footer full-bleed" role="contentinfo">
--- inc/scripts/header.inc.php
+++ inc/scripts/header.inc.php
@@ -5,9 +5,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Header</title>
 </head>
-<body>
-    <h1><em>Je suis un header</em></h1>
-</body>
+    <header class="header" role="banner">
+        <img class="header__logo" src="../../images/logoOff_noir.png" alt="accueil">
+        <ul class="nav">
+            <li class="nav__item"><a class="nav__lien">Le OFF</a></li>
+            <li class="nav__item"><a class="nav__lien">Programmation</a></li>
+            <li class="nav__item"><a class="nav__lien">Artistes</a></li>
+            <li class="nav__item"><a class="nav__lien">Partenaires</a></li>
+        </ul>
+        <button class="header__btn main__btn" type="button">Acheter mon passeport</button>
+    </header>
 </html>
