217aec4de9ef077882a2dc8ee2cfcaf366442087
Michel PC <0801342@csfoy.ca>
2022-10-31 15:40:47 -0400

    Ajustements des classes et balises HTML de la fiche artiste

--- artistes/fiche/index.php
+++ artistes/fiche/index.php
@@ -23,24 +23,25 @@
     <header><?php include($niveau . "inc/scripts/header.inc.php"); ?></header>
 
     <main id="main" role="main" class="main">
-        <h1>Nom Artiste</h1>
+        <h1 class="titrePrincipal">Nom Artiste</h1>
         <a href="">Site Web</a>
         <img src="https://i.picsum.photos/id/386/960/490.jpg?hmac=RcIYBU3QIXDOP7NMdRKxaWlzf3izkxtM81zazZgricw" alt="">
 
-        <h2>Description</h2>
+        <h2 class="description_h2">Description</h2>
         <p>La formation 3D KIDS existe depuis 2004. Puriste de punk '77, les 3D KIDS gardent ça simple et droit au but. X (guitare/voix), Y (batterie) et Z (basse) ont deux albums à leur actif (ZYX et Retarded Love) et un troisième est prévu avant la fin de l'année.</p>
 
-        <h2>Provenance</h2>
+        <h2 class="provenance_h2">Provenance</h2>
         <p>Québec</p>
 
-        <h2>Style musical</h2>
-        <p>Rock</p>
+        <h2 class="styleMusic_h2">Style musical</h2>
+        <p class="h2">Rock</p>
 
-        <h2>Représentations</h2>
-        <p>Salle Multi de Méduse, le jeudi 9 juillet à 22h30</p>
-        <p>Salle Multi de Méduse, le vendredi 10 juillet à 23h00</p>
-
-        <h2>Découvrir d'autres artistes</h2>
+        <h2 class="representation_h2">Représentations</h2>
+        <ul>
+            <li>Salle Multi de Méduse, le jeudi 9 juillet à 22h30</li>
+            <li>Salle Multi de Méduse, le vendredi 10 juillet à 23h00</li>
+        </ul>
+        <h2 class="h2">Découvrir d'autres artistes</h2>
         <div class="container">
             <ul class="artistes_ul">
             <?php
