2ccf7f7ceac0e5108a8a01b8e2054ba457ba35a0
Christopher Byatt <2064371@csfoy.ca>
2022-11-01 13:35:39 -0400

    css accueil mobile

--- Index.php
+++ Index.php
@@ -2,16 +2,92 @@
 
 ?>
 
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style-christopher.css">
 </head>
 <body>
-    <h1>TEST</h1>
+<header class="header" role="banner">
+    <img class="header__logo" src="images/logoOff_noir.png" alt="accueil">
+    <ul class="nav">
+        <li class="nav__item"><a class="nav__lien" href="#">Le OFF</a></li>
+        <li class="nav__item"><a class="nav__lien" href="#">Programmation</a></li>
+        <li class="nav__item"><a class="nav__lien" href="artistes/index.php">Artistes</a></li>
+        <li class="nav__item"><a class="nav__lien" href="#">Partenaires</a></li>
+    </ul>
+    <h1 class="h1">FESTIVAL OFF
+        du 8 au 11 juillet
+        TOUT EST POSSIBLE! </h1>
+    <button class="header__btn" type="button">Acheter mon passeport</button>
+</header>
+<main class="main" role="main">
+<div class="main__actu">
+    <h2 class="h2">ACTUALITÉS</h2>
+    <section class="main__sectionArticle">
+        <div class="main__article">
+            <h3 class="main__h3">Soirée à saveur internationale sur la rue Cartier</h3>
+            <p class="main__p">À 17 h 30, LaTourelle Orkestra (Québec) vous proposera leur entraînante musique du monde à saveur tsigane. À 19 h, Romain Malagnoux (Québec), habile guitariste à la voix posée, saura faire danser les passants de la rue Cartier. À 20 h 30, chanteuse romanti-comico-tragique originaire <a class="" href="#">...</a></p>
+            <h3 class="main__h3">Par Elisabeth et Karine, le mercredi 11 Aout 2023 16h30</h3>
+        </div>
+        <div class="main__article">
+            <h3 class="main__h3">Un apéro qui promet à la Scène Caisse populaire de Québec</h3>
+            <p class="main__p">À 17 h, le sextuor Harvest Breed (Sherbrooke) vous servira un apéro musical aux influences des années 60 et 70. À 19 h, Orkestar Kriminal (Montréal), une douzaine de musiciens puisant dans le répertoire yiddish, grec, danois, punk des années 1920 et 1930 prendront place<a class="" href="#">...</a></p>
+            <h3 class="main__h3">Par Elisabeth et Karine, le mercredi 11 Aout 2023 10h00</h3>
+        </div>
+        <div class="main__article">
+            <h3 class="main__h3">Débutez votre fin de semaine sur une note familiale à la Scène de la famille Télé-Québec</h3>
+            <p class="main__p">De 13 h à 19 h, profitez de ce lieu en famille. De nombreux kiosques, maquillage pour enfants, animation de cirque, jeux gonflables et bien plus. L’Express Rock’n’Roll sera également stationné et accueillera mélomanes et néophytes à son bord pour faire découvrir ce musée ambulant<a class="" href="#">...</a></p>
+            <h3 class="main__h3">Par Elisabeth et Karine, le mercredi 11 Aout 2023 8h33</h3>
+        </div>
+    </section>
+</div>
+<div class="main__artiste">
+    <h2 class="h2">EN VEDETTES</h2>
+    <section class="main__sectionArtiste">
+        <div class="main__artisteVedette">
+            <img src="" alt="">
+            <h3 class="main__h3Vedette">CABARET OLIBRIUS</h3>
+        </div>
+        <div class="main__artisteVedette">
+            <img src="" alt="">
+            <h3 class="main__h3Vedette">DIAMOND RINGS</h3>
+        </div>
+        <div class="main__artisteVedette">
+            <img src="" alt="">
+            <h3 class="main__h3Vedette">JAH & I</h3>
+        </div>
+    </section>
+</div>
+</main>
+<footer class="footer" role="contentinfo">
+    <div class="haut">
+        <img class="footer__logo" src="/images/off_jaune.svg" alt="accueil">
+        <p class="adresse">110 boulevard René-Lévesque Ouest<br>C.P. 48036<br>QC, Québec, G1R 5R5</p>
+    </div>
+    <ul class="nav">
+        <div class="logo-adresse">
+            <img class="logo-adresse__logo" src="/images/off_jaune.svg" alt="accueil">
+            <p class="logo-adresse__adresse">110 boulevard René-Lévesque Ouest<br>C.P. 48036<br>QC, Québec, G1R 5R5</p>
+        </div>
+        <ul class="footer--nav">
+            <li class="menu__item"><a class="nav__lien" href="#">Le OFF</a></li>
+            <li class="nav__item"><a class="nav__lien" href="#">Programmation</a></li>
+            <li class="nav__item"><a class="nav__lien" href="artistes/index.php">Artistes</a></li>
+            <li class="nav__item"><a class="nav__lien" href="#">Partenaires</a></li>
+        </ul>
+        <div class="informations">
+            <button class="informations__btn" type="button">Acheter mon passeport</button>
+            <button class="informations__btn main__btn" type="button">Acheter mon passeport</button>
+            <p class="informations__p">du 6 juillet au 10 juillet 2022</p>
+            <a class="information__lien" href="">Formulaire d’abonnement à l’infolettre du festival</a>
+            <div class="reseaux">
+                <a class="reseaux__lien" href="facebook.com"><img class="reseaux_img" src="" alt="facebook"></a>
+                <a class="reseaux__lien" href="twitter.com"><img class="reseaux_img" src="" alt="twitter"></a>
+                <a class="reseaux__lien" href="youtube.com"><img class="reseaux_img" src="" alt="youtube"></a>
+            </div>
+        </div>
+        <p class="footer__copyright">© 2009-2022 Festival OFF Tous droits réservés</p>
+</footer>
 </body>
 </html>
