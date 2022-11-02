9aaf2f8e51a59a56b415cbaf555a107f8a0851ab
amanneeb <2134649@csfoy.ca>
2022-11-02 13:42:44 -0400

    liste artiste: header et footer fini

--- js/script_liste_artiste.js
+++ js/script_liste_artiste.js
@@ -1,17 +1,1 @@
-refButtonPlus=document.querySelector(".plus");
-refButtonSavoirPlus=document.querySelector(".enSavoirPlus");
 
-document.querySelector(".infoArtiste__img").addEventListener("mouseover", function(){
-    refButtonPlus.removeAttribute("hidden");
-})
-
-document.querySelector(".infoArtiste__img").addEventListener("mouseout", function(){
-    refButtonPlus.setAttribute("hidden", "hidden");
-})
-
-document.querySelector(".plus").addEventListener("mouseover", function(){
-    refButtonSavoirPlus.removeAttribute("hidden");
-})
-document.querySelector(".plus").addEventListener("mouseout", function(){
-    refButtonSavoirPlus.setAttribute("hidden", "hidden");
-})
