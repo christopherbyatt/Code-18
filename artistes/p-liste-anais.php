dffe2b235194a2d2fbaf978678fa83a085aa70b9
amanneeb <2134649@csfoy.ca>
2022-11-04 12:34:27 -0400

    liste artiste 4nov 12h

--- artistes/p-liste-anais.php
+++ artistes/p-liste-anais.php
@@ -1,5 +1,125 @@
 <?php
-;
+ini_set("display_errors", 1);
+$niveau="../";
+include ("../inc/fragment/config.inc.php");
+
+//**********
+//Récupérer la page dans la querystring
+//**********
+if(isset($_GET["id_page"])==true){
+    $id_page= $_GET["id_page"];
+}else{
+    $id_page=1;
+}
+$nbArtisteParPage = 3;
+$enregistrementDepart = 3 * $id_page;
+$enregistrementDepartBis=(3 * $id_page)-2;
+
+//************
+//Récupérer le style dans la querystring + requête SQL principale
+//************
+$idStyle="";
+if(isset($_GET["id_style"])==true) {
+    $idStyle = $_GET["id_style"];
+    $strRequete="SELECT t_artiste.id_artiste, nom_artiste 
+                FROM t_artiste 
+                INNER JOIN ti_style_artiste ON t_artiste.id_artiste=ti_style_artiste.id_artiste 
+                INNER JOIN t_style ON t_style.id_style=ti_style_artiste.id_style 
+                WHERE ti_style_artiste.id_style=".$idStyle." 
+                ORDER BY nom_artiste LIMIT ".$enregistrementDepartBis.", ".$nbArtisteParPage;
+}else{
+    $idStyle = 0;
+    $strRequete="SELECT t_artiste.id_artiste, nom_artiste
+    FROM t_artiste
+    ORDER BY nom_artiste
+    LIMIT ".$enregistrementDepartBis.", ".$nbArtisteParPage;
+}
+
+//**********
+//Récupérer le nombre d'artiste
+//**********
+if($idStyle!=0){
+    $strRequeteCount="SELECT COUNT(*) AS nbEnregistrement
+        FROM t_artiste
+        INNER JOIN ti_style_artiste ON t_artiste.id_artiste=ti_style_artiste.id_artiste
+        WHERE ti_style_artiste.id_artiste=". $idStyle;
+}else{
+    $strRequeteCount= "SELECT COUNT(*) AS nbEnregistrement 
+                        FROM t_artiste";
+}
+$pdoResultatCpt=$pdoConnexion->query($strRequeteCount);
+$totalArtistes=$pdoResultatCpt->fetch();
+$nbArtistes=$totalArtistes["nbEnregistrement"];
+$pdoResultatCpt->closeCursor();
+$nbPages=ceil($nbArtistes/$nbArtisteParPage);
+//*************
+
+$pdosResultat=$pdoConnexion->prepare($strRequete);
+$pdosResultat->execute();
+$arrNoms=array();
+$ligne=$pdosResultat->fetch();
+for($intCptLigne=0;$intCptLigne<$pdosResultat->rowCount();$intCptLigne++){
+    $arrNoms[$intCptLigne]["id_artiste"]=$ligne["id_artiste"];
+    $arrNoms[$intCptLigne]["nom_artiste"]=$ligne["nom_artiste"];
+
+    //**********************
+    //Sous-requête
+    //**********************
+    $strSousRequete= "SELECT nom_style FROM t_style 
+                    INNER JOIN ti_style_artiste ON t_style.id_style = ti_style_artiste.id_style 
+                    WHERE ti_style_artiste.id_artiste=" .$arrNoms[$intCptLigne]["id_artiste"];
+
+    $pdosSousResultat=$pdoConnexion->prepare($strSousRequete);
+    $pdosSousResultat->execute();
+
+    $ligneStyles=$pdosSousResultat->fetch();
+    $strStyles="";
+    for($intCptStyles=0;$intCptStyles<$pdosSousResultat->rowCount();$intCptStyles++){
+        if($strStyles != ""){
+            $strStyles = $strStyles . ", ";
+        }
+        $strStyles = $strStyles. $ligneStyles["nom_style"];
+        $ligneStyles=$pdosSousResultat->fetch();
+    }
+    $pdosSousResultat->closeCursor();
+    //*****************************
+
+    $arrNoms[$intCptLigne]["artiste_style"] = $strStyles;
+    $ligne =$pdosResultat->fetch();
+}
+$pdosResultat->closeCursor();
+
+//***************
+//Récupérer les noms de styles de musique
+//***************
+$strRequete="SELECT id_style, nom_style 
+                    FROM t_style";
+$pdosResultat=$pdoConnexion->query($strRequete);
+$arrStyles=array();
+$ligne=$pdosResultat->fetch();
+for($intCptArrStyle=0;$ligne=$pdosResultat->fetch();$intCptArrStyle++){
+    $arrStyles[$intCptArrStyle]["id_style"]=$ligne["id_style"];
+    $arrStyles[$intCptArrStyle]["nom_style"]=$ligne["nom_style"];
+}
+$pdosResultat->closeCursor();
+//***************
+
+$strRequete="SELECT nom_artiste, id_artiste
+                FROM t_artiste";
+$pdosResultatArtSug=$pdoConnexion->query($strRequete);
+$arrArtistesSug=array();
+for($intCptLigne=0; $ligne=$pdosResultatArtSug->fetch()/*$intCptLigne<$pdosResultat->rowCount()*/;$intCptLigne++){
+    $arrArtistesSug[$intCptLigne]["nom_artiste"]=$ligne["nom_artiste"];
+    $arrArtistesSug[$intCptLigne]["id_artiste"]=$ligne["id_artiste"];
+}
+$pdosResultatArtSug->closeCursor();
+
+$arrArtistesChoisis=array();
+for($cpt2=0; $cpt2<=2; $cpt2++) {
+    $artisteChoisi=rand(0, count($arrArtistesSug)-1);
+    array_push($arrArtistesChoisis, $arrArtistesSug[$artisteChoisi]);
+    array_splice($arrArtistesSug, $artisteChoisi, 1);
+}
 ?>
 
 <!DOCTYPE html>
@@ -34,13 +154,25 @@
 <main class="main" role="main">
     <h1 class="titrePrincipal">Liste <br> des <br> artistes</h1>
     <section class="artistes">
+
         <div class="ctn-infoArtiste">
+            <?php
+            if($idStyle !=0){
+                echo "<p> Style: " .$arrStyles[$idStyle-2]["nom_style"]. "</p>";
+            }
+            ?>
+        <?php for($intCpt=0;$intCpt<count($arrNoms);$intCpt++){ ?>
+
             <figure class="infoArtiste">
-                <img class="infoArtiste__img" src="../images/placeholder.png" alt="Cabaret Olibrius">
-                <div class="btnImages btnImages--jaune"></div>
-                <figcaption class="infoArtiste__nom nom--un"> <a href="">Cabaret Olibrius</a> </figcaption>
+                <img class="infoArtiste__img" src="../images/placeholder.png" alt="<?php echo $arrNoms[$intCpt]["nom_artiste"] ?>">
+                <div class="btnImages "></div>
+                <figcaption class="infoArtiste__nom"> <a class="infoArtiste__nom__lien" href="<?php echo $niveau."artistes/fiche/p-fiche-prenom.php?id_artiste=".$arrNoms[$intCpt]["id_artiste"]; ?>"><?php echo $arrNoms[$intCpt]["nom_artiste"] ?><!<--Cabaret Olibrius-->
+                        <br> <span class="infoArtiste__nom__span">Style(s): <?php echo $arrNoms[$intCpt]["artiste_style"]; ?></span>
+                    </a>
+                </figcaption>
             </figure>
-            <figure class="infoArtiste">
+        <?php } ?>
+        <!--    <figure class="infoArtiste">
                 <img class="infoArtiste__img" src="../images/placeholder.png" alt="Diamond Rings">
                 <div class="btnImages btnImages--orange"></div>
                 <figcaption class="infoArtiste__nom nom--deux">Diamond Rings</figcaption>
@@ -49,11 +181,59 @@
                 <img class="infoArtiste__img" src="../images/placeholder.png" alt="Jah & I">
                 <div class="btnImages btnImages--bleu"></div>
                 <figcaption class="infoArtiste__nom nom--trois">Jah & I</figcaption>
-            </figure>
+            </figure> -->
         </div>
         <div class="ctn-btn">
-            <button class="arriere" type="button">&#9664;</button>
-            <button class="avant" type="button">&#9654;</button>
+            <?php
+            if($idStyle==0){
+                if($id_page>1){
+            ?>
+            <a href='p-liste-anais.php?id_page=<?php echo ($id_page-1); ?>' class="arriere" >&#9664;</a>
+            <?php }
+            } else { ?>
+
+            <a href='p-liste-anais.php?id_page=<?php echo ($id_page-1); ?>&id_style=<?php echo $idStyle; ?>' class="arriere" >&#9664;</a>
+            <?php }
+            ?>
+
+            <?php
+            if($idStyle==0){
+            if($id_page<$nbPages){
+            ?>
+                <a href='p-liste-anais.php?id_page=<?php echo ($id_page+1); ?>' class="avant">&#9654;</a>
+            <?php }
+            } else { ?>
+                <a href='p-liste-anais.php?id_page=<?php echo ($id_page+1); ?>&id_style=<?php echo $idStyle; ?>' class="avant">&#9654;</a>
+            <?php }
+            ?>
+        </div>
+    </section>
+
+    <h2 class="titreSecondaire">En vedette:</h2>
+
+        <ul class="ctn-suggestionArtiste">
+            <?php for($cpt=0;$cpt<count($arrArtistesChoisis); $cpt++) { ?>
+                <li class="ctn-suggestionArtiste__item">
+                    <figure class="artisteSugg">
+                        <img class="artisteSugg__img" src="../images/mini_placeholder.png" alt="<?php echo $arrArtistesChoisis[$cpt]["nom_artiste"]; ?>">
+                        <figcaption class="artisteSugg__figcap">
+                            <a class="artisteSugg__figcap__lien" href='<?php echo $niveau ?>artistes/fiche/p-fiche-prenom.php?id_artiste=<?php echo $arrArtistesChoisis[$cpt]["id_artiste"];?>'><?php echo $arrArtistesChoisis[$cpt]["nom_artiste"]; ?></a>
+                        </figcaption>
+                    </figure>
+                </li>
+            <?php } ?>
+        </ul>
+
+    <section class="styles">
+        <div>
+            <h3 class="titreSecondaireBis">Tous les styles</h3>
+            <ul class="listeStyle">
+                <?php
+                for($intCpt1=0;$intCpt1<count($arrStyles);$intCpt1++){ ?>
+                    <li class="listeStyle__item"><a class="listeStyle__lien" href='<?php echo "?id_style=".$arrStyles[$intCpt1]["id_style"]?>'><?php echo $arrStyles[$intCpt1]["nom_style"]?></a></li>
+                <?php }
+                ?>
+            </ul>
         </div>
     </section>
 </main>
@@ -64,10 +244,10 @@
         <p class="logo-adresse__adresse">110 boulevard René-Lévesque Ouest<br>C.P. 48036<br>QC, Québec, G1R 5R5</p>
     </div>
     <ul class="footerNav">
-        <li class="footerNav__item"><a class="footerNav__lien">Le OFF</a></li>
-        <li class="footerNav__item"><a class="footerNav__lien">Programmation</a></li>
-        <li class="footerNav__item"><a class="footerNav__lien">Artistes</a></li>
-        <li class="footerNav__item"><a class="footerNav__lien">Partenaires</a></li>
+        <li class="footerNav__item"><a href="" class="footerNav__lien">Le OFF</a></li>
+        <li class="footerNav__item"><a href="" class="footerNav__lien">Programmation</a></li>
+        <li class="footerNav__item"><a href="" class="footerNav__lien">Artistes</a></li>
+        <li class="footerNav__item"><a href="" class="footerNav__lien">Partenaires</a></li>
     </ul>
     <div class="informations">
         <a href="" class="informations__btn main__btn" type="button">Acheter mon passeport</a>
