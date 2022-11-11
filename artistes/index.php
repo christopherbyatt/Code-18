2f5a83b3d6e3604282e9735b7c22e0efd2ed12c4
amanneeb <2134649@csfoy.ca>
2022-11-11 12:26:54 -0500

    ajustements page artiste

--- artistes/index.php
+++ artistes/index.php
@@ -125,28 +125,13 @@ for($cpt2=0; $cpt2<=2; $cpt2++) {
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/style-anais.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0," />
 </head>
 <body>
-<header class="header" role="banner">
-    <img class="header__logo" src="../images/logoOff_bleu_fonce.svg" alt="accueil">
-    <ul class="nav">
-        <li class="nav__item nav__item--un">
-            <a href="../Index.php" class="nav__lien">Le OFF</a>
-        </li>
-        <li class="nav__item nav__item--deux">
-            <a href="" class="nav__lien">Programmation</a>
-        </li>
-        <li class="nav__item nav__item--trois">
-            <a href="index.php" class="nav__lien">Artistes</a>
-        </li>
-        <li class="nav__item nav__item--quatre">
-            <a href="" class="nav__lien">Partenaires</a>
-        </li>
-    </ul>
-    <button class="header__btn main__btn" type="button">Acheter mon passeport</button>
-</header>
+<?php include("../inc/scripts/header.inc.php")?>
+
 <main class="main" role="main">
     <h1 class="titrePrincipal">Liste <br> des <br> artistes</h1>
     <section class="artistes">
@@ -223,30 +208,8 @@ for($cpt2=0; $cpt2<=2; $cpt2++) {
         </div>
     </section>
 </main>
+<?php include("../inc/scripts/footer.inc.php")?>
 
-<footer class="footer" role="contentinfo">
-    <div class="logo-adresse">
-        <img class="logo-adresse__logo" src="../images/logoOff_jaune.svg" alt="accueil">
-        <p class="logo-adresse__adresse">110 boulevard René-Lévesque Ouest<br>C.P. 48036<br>QC, Québec, G1R 5R5</p>
-    </div>
-    <ul class="footerNav">
-        <li class="footerNav__item"><a href="" class="footerNav__lien">Le OFF</a></li>
-        <li class="footerNav__item"><a href="" class="footerNav__lien">Programmation</a></li>
-        <li class="footerNav__item"><a href="" class="footerNav__lien">Artistes</a></li>
-        <li class="footerNav__item"><a href="" class="footerNav__lien">Partenaires</a></li>
-    </ul>
-    <div class="informations">
-        <a href="" class="informations__btn main__btn" type="button">Acheter mon passeport</a>
-        <p class="informations__p">du 6 juillet au 10 juillet 2022</p>
-        <a class="information__lien" href="">Formulaire d’abonnement à l’infolettre du festival</a>
-        <div class="reseaux">
-            <a class="reseaux__lien" href="https://www.facebook.com"><img class="reseaux__img" src="../images/reseaux/facebook.svg" alt="facebook"></a>
-            <a class="reseaux__lien" href="https://www.twitter.com"><img class="reseaux__img" src="../images/reseaux/twitter.svg" alt="twitter"></a>
-            <a class="reseaux__lien" href="https://www.youtube.com"><img class="reseaux__img" src="../images/reseaux/youtube.svg" alt="youtube"></a>
-        </div>
-    </div>
-    <p class="footer__copyright">© 2009-2022 Festival OFF Tous droits réservés</p>
-</footer>
 <script src="../js/script_liste_artiste.js"></script>
 </body>
 </html>
